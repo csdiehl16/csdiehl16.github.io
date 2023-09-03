8a2fa8ba5575f76de71a4d4cd5973a3b63b455ad
Caleb Diehl <cdiehl@ap.org>
2023-09-03 11:56:36 -0700

    scroll-snap

--- src/App.js
+++ src/App.js
@@ -12,33 +12,22 @@ function App() {
 
   const projectsRef = useRef()
 
-  const scroll = () => {
-    projectsRef.current.scrollIntoView({ behavior: "smooth" })
-  }
-
   const navClickHandler = (event) => {
     setActivePage(event.target.value)
   }
 
   return (
     <div className="App" style={{ width: "100%" }}>
-      <Profile
-        setActivePage={setActivePage}
-        activePage={activePage}
-        scroll={scroll}
-      />
-      <div
-        style={{ height: "calc(100vh - 68px)", background: "transparent" }}
-      ></div>
+      <Profile setActivePage={setActivePage} activePage={activePage} />
       <div
         style={{
-          background: "#171717",
-          zIndex: 10,
-          position: "relative",
-          transform: "translate3d(0, 0, 0)",
-          width: "100%",
+          height: "calc(100vh - 68px)",
+          background: "transparent",
+          scrollSnapAlign: "start",
+          scrollSnapStop: "normal",
         }}
-      >
+      ></div>
+      <div className="main-container">
         <div className="nav-bar">
           {pages.map((page) => (
             <div key={page}>
