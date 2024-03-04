556deee8017c69def0b3f2a209117871449cc55d
Caleb Diehl <cdiehl@ap.org>
2024-03-03 20:52:22 -0800

    fix arrow path

--- src/components/ProjectTile.jsx
+++ src/components/ProjectTile.jsx
@@ -1,26 +1,43 @@
-import classes from './ProjectTile.module.css';
-import { useInView } from 'react-intersection-observer';
+import classes from "./ProjectTile.module.css"
+import { useInView } from "react-intersection-observer"
 
 const ProjectTile = (props) => {
-    const { ref, inView } = useInView({ threshold: 0.6 });
+  const { ref, inView } = useInView({ threshold: 0.6 })
 
-    return (
-        <div ref={ref} className={inView ? classes.tile : `${classes.tile} ${classes['tile-hidden']}`}>
-            <div>
-                <h2 className={classes['project-title']}>{props.title}</h2>
-                <p className={classes['description']}>{props.description}</p>
-                <p className={classes['language-tag']}>{props.languageTag.join(' / ')}</p>
-                <a className={classes['project-link']} href={props.link}>
-                    <span>View Project</span>
-                    <img alt="right-arrow" src="./arrow-right.svg"></img>
-                </a>
-            </div>
-            <a href={props.link}>
-                <img className={classes['tile-img']} src={props?.image} alt={props.title}></img>
-            </a>
-            {props.badge && <img className={classes['badge']} alt="information is beautiful awards badge" src={props.badge}></img>}
-        </div>
-    );
-};
+  return (
+    <div
+      ref={ref}
+      className={
+        inView ? classes.tile : `${classes.tile} ${classes["tile-hidden"]}`
+      }
+    >
+      <div>
+        <h2 className={classes["project-title"]}>{props.title}</h2>
+        <p className={classes["description"]}>{props.description}</p>
+        <p className={classes["language-tag"]}>
+          {props.languageTag.join(" / ")}
+        </p>
+        <a className={classes["project-link"]} href={props.link}>
+          <span>View Project</span>
+          <img alt="right-arrow" src="/arrow-right.svg"></img>
+        </a>
+      </div>
+      <a href={props.link}>
+        <img
+          className={classes["tile-img"]}
+          src={props?.image}
+          alt={props.title}
+        ></img>
+      </a>
+      {props.badge && (
+        <img
+          className={classes["badge"]}
+          alt="information is beautiful awards badge"
+          src={props.badge}
+        ></img>
+      )}
+    </div>
+  )
+}
 
-export default ProjectTile;
+export default ProjectTile
