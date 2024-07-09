1baf16b6a53b32c3376f80fb8e9ed4f7b0e98c99
Caleb Diehl <cdiehl@ap.org>
2024-07-08 17:26:15 -0700

    fix build errors

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,33 +1,39 @@
-import ProjectList from './ProjectList';
-import { useState, useRef } from 'react';
-import classes from './Projects.module.css';
+import ProjectList from "./ProjectList"
+import { useState, useRef } from "react"
+import classes from "./Projects.module.css"
 
-const projectTypes = ['All', 'Geospatial', 'Dashboards', 'Data journalism', 'Web development'];
+const projectTypes = [
+  "All",
+  "Geospatial",
+  "Dashboards",
+  "Data journalism",
+  "Web development",
+]
 
 function Projects() {
-    const [filters, setFilters] = useState('All');
+  const [filters, setFilters] = useState("All")
 
-    const projectsRef = useRef();
+  const projectsRef = useRef()
 
-    return (
-        <div className={classes['App']} style={{ width: '100%' }}>
-            <div className={classes['nav-bar']}>
-                {projectTypes.map((d) => (
-                    <button
-                        onClick={() => setFilters(d)}
-                        className={`${classes['nav-btn']} ${classes[filters === d ? 'nav-btn-clicked' : 'project-button']}`}
-                        key={d}
-                    >
-                        {d}
-                    </button>
-                ))}
-            </div>
-            <div className={classes['nav-line']}>
-                <div className={classes['nav-highlight']}></div>
-            </div>
-            <ProjectList filters={filters} ref={projectsRef} />
-        </div>
-    );
+  return (
+    <div className={classes["App"]} style={{ width: "100%" }}>
+      <div className={classes["nav-bar"]}>
+        {projectTypes.map((d) => (
+          <button
+            onClick={() => setFilters(d)}
+            className={`${classes["nav-btn"]} ${classes[filters === d ? "nav-btn-clicked" : "project-button"]}`}
+            key={d}
+          >
+            {d}
+          </button>
+        ))}
+      </div>
+      <div className={classes["nav-line"]}>
+        <div className={classes["nav-highlight"]}></div>
+      </div>
+      <ProjectList filters={filters} ref={projectsRef} />
+    </div>
+  )
 }
 
-export default Projects;
+export default Projects
