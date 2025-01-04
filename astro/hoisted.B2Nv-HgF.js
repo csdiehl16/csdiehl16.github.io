077f65aa1cef7bc371da339fa84e4e61e19ce6d1
Caleb Diehl <csdiehl@Calebs-MacBook-Pro.local>
2025-01-04 15:47:00 -0800

    Updates

---
+++ astro/hoisted.B2Nv-HgF.js
@@ -0,0 +1,1 @@
+import"./ViewTransitions.astro_astro_type_script_index_0_lang.C9K5sHun.js";localStorage.theme==="dark"&&document.documentElement.classList.add("dark");
--- astro/hoisted.IJOW2Jjz.js
+++
@@ -1,1 +0,0 @@
-import"./ViewTransitions.astro_astro_type_script_index_0_lang.ZJAMyYUk.js";localStorage.theme==="dark"&&document.documentElement.classList.add("dark");
